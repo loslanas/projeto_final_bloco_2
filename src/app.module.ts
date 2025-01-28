6119e97d1c3bcc53a6fb6d10d7f8fb22a573c9c2
Fernando Lana <loslanas@gmail.com>
2025-01-28 11:41:38 -0300

    :tada: Primeiro Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     port: 3306,
     username: 'root',
     password: 'root',
-    database: '', // Cadastrar o nome do BD
-    entities: [], // Cadastrar as Classes Entities, para que o 
+    database: 'db_farmacia', // Cadastrar o nome do BD
+    autoLoadEntities: true, // Cadastrar as Classes Entities, para que o 
     // TypeORM possa gerar as tabelas correspondentes no BD
     synchronize: true,
     logging: true
